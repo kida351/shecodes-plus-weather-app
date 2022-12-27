a0e4bb950a53a17a8e838cc4528503402db3f66d
kida351 <kida.sarchese@gmail.com>
2022-12-27 17:44:06 -0500

    Added temperature conversion

--- index.js
+++ index.js
@@ -39,12 +39,11 @@ let cityForm2 = document.querySelector("#city-form");
 cityForm2.addEventListener("submit", citySearch);
 
 function showTemperature(response) {
-  document.querySelector("#current-temp").innerHTML = Math.round(
-    response.data.temperature.current
-  );
+  tempC = response.data.temperature.current;
+
+  document.querySelector("#current-temp").innerHTML = Math.round(tempC);
   document.querySelector("#city-input").innerHTML = response.data.name;
   document.querySelector("h5").innerHTML = response.data.condition.description;
-
   document.querySelector("#feels-like").innerHTML = Math.round(
     response.data.temperature.feels_like
   );
@@ -57,3 +56,29 @@ function showTemperature(response) {
     .querySelector("#big-icon")
     .setAttribute("src", response.data.condition.icon_url);
 }
+
+//conversion
+function displayTempF(event) {
+  event.preventDefault();
+  tempClink.classList.remove("active");
+  tempFlink.classList.add("active");
+  let tempF = (tempC * 9) / 5 + 32;
+  let temperatureElement = document.querySelector("#current-temp");
+  temperatureElement.innerHTML = Math.round(tempF);
+}
+
+function displayTempC(event) {
+  event.preventDefault();
+  tempFlink.classList.remove("active");
+  tempClink.classList.add("active");
+  let temperatureElement = document.querySelector("#current-temp");
+  temperatureElement.innerHTML = Math.round(tempC);
+}
+
+let tempC = null;
+
+let tempFlink = document.querySelector("#tempf-link");
+tempFlink.addEventListener("click", displayTempF);
+
+let tempClink = document.querySelector("#tempc-link");
+tempClink.addEventListener("click", displayTempC);
